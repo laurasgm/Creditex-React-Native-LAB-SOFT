4cb22ed1a79ebf0f43e85534fd5865ab89322e09
laurasgm <laurasophia.gonzalez@utp.edu.co>
2019-09-23 21:34:39 -0500

    primera vista

---
+++ App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Card, CardItem, Body, Text, Left, Right, Title, View, Fab, Button, Icon} from 'native-base';
+
+const dataArray = [
+  { title: "Bancolombia"},
+  { title: "Banco de Bogota"},
+  { title: "Davivienda"}
+];
+
+export default class CardExample extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false
+    };
+  }
+
+  
+
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left />
+            <Body>
+              <Title>Creditex</Title>
+            </Body>
+          <Right />
+        </Header>
+
+
+        <Content>
+          <Card>
+            <CardItem>
+              <Body>
+                <Text>dataArray={dataArray}</Text>
+              </Body>
+            </CardItem>
+          </Card>
+        </Content>
+
+
+        <View style={{ flex: 1 }}>
+          <Fab
+            active={this.state.active}
+            direction="up"
+            containerStyle={{ }}
+            style={{ backgroundColor: '#5067FF' }}
+            position="bottomRight"
+            onPress={() => this.setState({ active: !this.state.active })}>
+            <Icon name="add" />
+          </Fab>
+        </View>
+      </Container>
+    );
+  }
+}
